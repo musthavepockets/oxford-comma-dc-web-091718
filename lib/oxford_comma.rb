8116ac167634b8bcacf05e8f9987e98da07cf757
cd <coraldarling@outlook.com>
2018-09-04 16:03:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2 
+   array.join(" and ")
+  else
+    new_array = array.insert(-2, "and ")
+    new_array[0..-2].join(", ") + new_array[-1]
+    
+  end
 end
