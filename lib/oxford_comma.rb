58e75d9fda3029f55d76c4ac35292a08a59eddcb
cd <coraldarling@outlook.com>
2018-09-04 15:40:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,10 @@ def oxford_comma(array)
   if array.size == 1
     array.join
   elsif array.size == 2 
-   
+   array.join(" and ")
+  else
+    array[0..-2].join(", ")
+    array.insert(-2, and)
   end  
 end
 
