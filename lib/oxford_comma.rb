5038b1890c5c8d60304e491ae6423850454fb48f
cd <coraldarling@outlook.com>
2018-09-04 15:19:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
    new_array = array.insert(-2, "and")
    new_array.join" "
   else 
-    array[0..-2].each do |fruit| puts #{fruit, }
+    new_array[0..-2].each do |fruit| puts #{fruit, }
     end
   end  
 end
